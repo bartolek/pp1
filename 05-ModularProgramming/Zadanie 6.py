62f38d781a930004271fe2475ad7efb1ec3704c4
Bartosz Laskowsk <laskowski.bartosz99@gmail.com>
2019-11-22 22:55:33 +0100

    2

--- 05-ModularProgramming/Zadanie 6.py
+++ 05-ModularProgramming/Zadanie 6.py
@@ -1,29 +1,27 @@
 import csv
-from statistics import mean
+import statistics 
+with open('employees.csv', newline='') as f:
+    reader = csv.reader(f)
+    i = 0
+    for row in reader:
+        if i == 0:
+            print(f"X  {row[0].upper()}  {row[1].upper()}  {row[2].upper()}  {row[3].upper()}")
+            print("="*60)
+            i+=1
 
-mode = input('Zadanie A czy B? ')
+        else:
+            print(f"{i} {row[0]}  {row[1]}  {row[2]}  {row[3]}")
+            i+=1
+    print("="*60)
+    
+with open('employees.csv', newline='') as f:
+    reader = csv.reader(f)
+    wiek = []
+    a = 0
+    for row in reader:
+        if a!=0:
+            wiek.append(int(row[2]))
+        a+=1
+    print(f"Średni wiek: {statistics.mean(wiek)}")
+        
 
-if mode == 'a':
-    with open('employees.csv', newline='') as f:
-        reader = csv.reader(f)
-        c = 0
-        for row in reader:
-            if c == 0:
-                print('{:<5}{:<15}{:<15}{:<6}{:<34}'.format(
-                    '#', row[0].upper(), row[1].upper(), row[2].upper(), row[3].upper()))
-                line = '='*75
-                print(line)
-            else:
-                print('{:<5}{:<15}{:<15}{:<6}{:<25}'.format(
-                    c, row[0], row[1], row[2], row[3]))
-            c += 1
-else:
-    with open('employees.csv', newline='') as f:
-        reader = csv.reader(f)
-        c, ages = 0, []
-        for row in reader:
-            if c != 0:
-                ages.append(row[2])
-            c += 1
-        ages = [int(x) for x in ages]
-        print(mean(ages))
--- 05-ModularProgramming/shapes.py
+++ 05-ModularProgramming/shapes.py
@@ -1,9 +1,68 @@
 import turtle
 
-def drawSquare(x,y,n):
+def drawSquare(x,y,m):
     turtle.penup()
     turtle.setposition(x,y)
     turtle.pendown()
     for i in range(0,4):
         turtle.right(90)
-        turtle.forward(n)
+        turtle.forward(m)
+        
+def drawTriangle(x,y,m):
+    turtle.fillcolor("green")
+    turtle.begin_fill()
+    for i in range(3):
+        turtle.forward(m)
+        turtle.left(120)
+    turtle.end_fill()
+        
+def drawCircle(x,y,n):
+    turtle.penup()
+    turtle.setposition(x,y)
+    turtle.pendown()
+    turtle.circle(n)
+    
+def drawStar():
+    for i in range(10):
+        if i%2==0:
+            turtle.left(72)
+            turtle.forward(90)
+        else:
+            turtle.right(144)
+            turtle.forward(90)
+            
+def drawCross(x,y,m):
+    z = x
+    turtle.speed(9999)
+    for i in range(8):
+        for j in range(8):
+            if i%2==0:
+                if j%2==0:
+                    turtle.fillcolor("black")
+                    turtle.begin_fill()
+                    drawSquare(x,y,m)
+                    turtle.end_fill()
+                    x+=m
+                elif j%2!=0:
+                    drawSquare(x,y,m)
+                    x+=m
+            elif i%2!=0:
+                if j%2==0:  
+                    drawSquare(x,y,m)
+                    x+=m
+                elif j%2!=0:
+                    turtle.fillcolor("black")
+                    turtle.begin_fill()
+                    drawSquare(x,y,m)
+                    turtle.end_fill()
+                    x+=m
+        x=z
+        y+=m
+
+
+            
+drawCross(-150,-150,50)
+        
+
+
+
